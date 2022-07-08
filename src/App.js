7f79d61ce5c1d765c91b7d8f05afc9c11e0950f3
Almir-Araujo <araujo.almir@gmail.com>
2022-07-08 15:11:14 -0300

    Criação de arquivo ContextAPI e Services para Requisição da API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './App.css';
+import fetchPlants from './Services/starWarsPlantesAPI';
 
 function App() {
+  fetchPlants();
+
   return (
     <span>Hello, App!!</span>
   );
